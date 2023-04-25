82c09dc9e06adc433bed359d7f95d9bb5ac2aa1e
minazaher <menazaher115@gmail.com>
2023-04-25 02:45:24 +0200

    cart is functional from category page and single product page

--- src/main/java/com/example/ecommerceproject/Controller/CartController.java
+++ src/main/java/com/example/ecommerceproject/Controller/CartController.java
@@ -1,34 +1,24 @@
 package com.example.ecommerceproject.Controller;
 
 import com.example.ecommerceproject.Model.Product;
-import com.example.ecommerceproject.Service.ProductService;
 import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashSet;
 import java.util.Set;
 
-@RestController
+@Controller
 @RequestMapping("/cart")
-@RequiredArgsConstructor
-public class CartController{
-    private final ProductService productService;
+public class CartController {
 
-    @PostMapping("/add/{productId}")
-    public void addProductToCart(HttpSession session,@PathVariable String productId){
+    @GetMapping
+    public String viewCart(Model model, HttpSession session){
         Set<Product> cart = (Set<Product>) session.getAttribute("cart");
-        if (cart == null){
-            cart = new HashSet<>();
-            session.setAttribute("cart", cart);
-        }
-        Long product_id = Long.parseLong(productId);
-        Product product = productService.getProductsById(product_id);
-        cart.add(product);
-        System.out.println(cart.toString());
+        model.addAttribute("cart", cart);
+        return "cart";
     }
+
+
 }
